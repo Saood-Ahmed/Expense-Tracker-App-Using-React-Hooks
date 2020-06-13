7fe46b4b2c39e8de72ef270bf6670d29c860afa7
Saood-Ahmed <59747007+Saood-Ahmed@users.noreply.github.com>
2020-06-13 16:51:57 +0500

    Update Balance.js

--- src/Components/Balance.js
+++ src/Components/Balance.js
@@ -7,11 +7,12 @@ export const Balance = () => {
 
     const context = React.useContext(GlobalContext)
     const amounts = context.transactions.map((transaction => parseInt(transaction.amount)));
+    var total;
     if(amounts.length > 1) {
-        var total = amounts.reduce((prevItem, item) => (prevItem + item)).toFixed(2);
+        total = amounts.reduce((prevItem, item) => (prevItem + item)).toFixed(2);
     }
     else {
-        var total = 0;
+        total = 0;
     }
     return(
         <div style={{backgroundColor:"#fddb27ff",color:'black', textAlign:'center', border:"2px solid black", borderRadius:"1%"}}>
